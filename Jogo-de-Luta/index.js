34101cf1417e8c1503333769f2a13c77fd59d2d6
alvalenda <banysan@msn.com>
2022-06-23 18:03:38 -0300

    ultis js added

--- Jogo-de-Luta/index.js
+++ Jogo-de-Luta/index.js
@@ -16,27 +16,6 @@ const background = new Sprite({
     imageSrc: './assets/images/background.png',
 });
 
-const keys = {
-    a: {
-        pressed: false,
-    },
-    d: {
-        pressed: false,
-    },
-    w: {
-        pressed: false,
-    },
-    ArrowRight: {
-        pressed: false,
-    },
-    ArrowLeft: {
-        pressed: false,
-    },
-    ArrowUp: {
-        pressed: false,
-    },
-};
-
 const player = new Fighter({
     position: { x: 200, y: 0 },
     velocity: { x: 0, y: 0 },
@@ -59,43 +38,27 @@ const enemy = new Fighter({
     },
 });
 
-function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-            rectangle2.position.x &&
-        rectangle1.attackBox.position.x <=
-            rectangle2.position.x + rectangle2.width &&
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-            rectangle2.position.y &&
-        rectangle1.attackBox.position.y <=
-            rectangle2.position.y + rectangle2.height
-    );
-}
+const keys = {
+    a: {
+        pressed: false,
+    },
+    d: {
+        pressed: false,
+    },
+    w: {
+        pressed: false,
+    },
+    ArrowRight: {
+        pressed: false,
+    },
+    ArrowLeft: {
+        pressed: false,
+    },
+    ArrowUp: {
+        pressed: false,
+    },
+};
 
-function determineWinner({ player, enemy, timerId }) {
-    clearTimeout(timerId);
-    document.querySelector('#displayText').style.display = 'flex';
-    if (player.health === enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'EMPATE';
-    } else if (player.health > enemy.health) {
-        document.querySelector('#displayText').innerHTML =
-            'VITÓRIA DO JOGADOR 1';
-    } else {
-        document.querySelector('#displayText').innerHTML =
-            'VITÓRIA DO JOGADOR 2';
-    }
-}
-let timer = 100;
-let timerId;
-function decreaseTimer() {
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000);
-        timer--;
-        document.querySelector('#timer').innerHTML = timer;
-    } else {
-        determineWinner({ player, enemy, timerId });
-    }
-}
 decreaseTimer();
 
 function animate() {
