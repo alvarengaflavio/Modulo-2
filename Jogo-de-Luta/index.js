8a95b61b8a6ad34cc6cf631bae2cba19e649a6c6
alvalenda <banysan@msn.com>
2022-06-23 16:35:27 -0300

    implementando timer

--- Jogo-de-Luta/index.js
+++ Jogo-de-Luta/index.js
@@ -121,6 +121,16 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     );
 }
 
+let timer = 10;
+function decreaseTimer() {
+    if (timer > 0) {
+        setTimeout(decreaseTimer, 1000);
+        timer--;
+        document.querySelector('#timer').innerHTML = timer;
+    }
+}
+decreaseTimer();
+
 function animate() {
     window.requestAnimationFrame(animate);
     ctx.fillStyle = 'black';
