973b26a8b9390b0ec9e7e159f083912a3ca1550b
alvalenda <banysan@msn.com>
2022-06-23 17:41:08 -0300

    background added, classes as module

--- Jogo-de-Luta/index.js
+++ Jogo-de-Luta/index.js
@@ -7,6 +7,15 @@ canvas.height = 576;
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './assets/images/background.png',
+});
+
 const keys = {
     a: {
         pressed: false,
@@ -28,65 +37,7 @@ const keys = {
     },
 };
 
-class Sprite {
-    constructor({ position, velocity, color = 'red', offset }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 50;
-        this.height = 150;
-        this.lastKey;
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y,
-            },
-            offset,
-            width: 100,
-            height: 50,
-        };
-        this.color = color;
-        this.isAttacking = false;
-        this.health = 100;
-    }
-
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-        // attack box
-        if (this.isAttacking) {
-            ctx.fillStyle = 'green';
-            ctx.fillRect(
-                this.attackBox.position.x,
-                this.attackBox.position.y,
-                this.attackBox.width,
-                this.attackBox.height,
-            );
-        }
-    }
-
-    update() {
-        this.draw();
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
-
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        this.position.y + this.height + this.velocity.y >= canvas.height
-            ? (this.velocity.y = 0) // se Sprite no chão vy = 0, senão acelera g
-            : (this.velocity.y += gravity);
-    }
-
-    attack() {
-        this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-        }, 150);
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: { x: 200, y: 0 },
     velocity: { x: 0, y: 0 },
     offset: {
@@ -95,7 +46,7 @@ const player = new Sprite({
     },
 });
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: canvas.width - 200,
         y: 0,
@@ -151,7 +102,7 @@ function animate() {
     window.requestAnimationFrame(animate);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    background.update();
     player.update();
     enemy.update();
 
