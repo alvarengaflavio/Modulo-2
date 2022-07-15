b19580f56c68387ea500498f0d250d35463e4974
alvalenda <banysan@msn.com>
2022-07-15 19:32:33 -0300

    aula 13 fim

--- Aula13/script.js
+++ Aula13/script.js
@@ -21,6 +21,41 @@ async function getCharacters() {
           `,
         );
     });
+    let page = 1;
+
+    async function getCharacters() {
+        const resp = await fetch(
+            `https://rickandmortyapi.com/api/character?page=${page}`,
+        );
+        const data = await resp.json();
+
+        data.results.forEach(character => {
+            document.querySelector('#character-list').insertAdjacentHTML(
+                'beforeend',
+                `<div class="card">
+                <img class="image-character" src=${
+                    character.image
+                } alt=${`Imagem do ${character.name}`} />
+                <div>
+                    <h2 class="name-character">${character.name}</h2>
+                    <p class="species-character">${character.species}</p>
+                    <h4>Gender</h4>
+                    <p class="species-character">${character.gender}</p>
+                    <h4>Origin</h4>
+                    <p class="species-origin">${character.origin.name}</p>
+                </div>
+            </div>
+            `,
+            );
+        });
+    }
+
+    getCharacters();
+
+    function viewMore() {
+        page++;
+        getCharacters();
+    }
 }
 
 getCharacters();
